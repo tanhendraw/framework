defd002240e1ebb9aae3b92a151774c4b6e0d8ae
Tan Hendra <tanhendra.w@gmail.com>
2017-04-24 20:19:34 +0700

    fix checkbox widget

--- resources/views/admin/widget/checkbox.blade.php
+++ resources/views/admin/widget/checkbox.blade.php
@@ -1,9 +1,9 @@
 
 <div class="checkbox">
     @if(isset($data))
-        <label for="{{$field}}" class="text-capitalize"> <input type="checkbox" name="{!! $field !!}" id="{{$field}}" value="1" {{$attr or ''}} @if($data[$field] == 1) checked @endif>{{$title or $field}} </label>
+        <label for="{{$field}}" class="text-capitalize"> <input type="checkbox" value="1" name="{!! $field !!}" id="{{$field}}" {{$attr or ''}} @if($data[$field] == 1) checked @endif>{{$title or $field}} </label>
     @else
-        <label for="{{$field}}" class="text-capitalize"> <input type="checkbox" name="{!! $field !!}" id="{{$field}}" value="1" {{$attr or ''}}>{{$title or $field}} </label>
+        <label for="{{$field}}" class="text-capitalize"> <input type="checkbox" value="1" name="{!! $field !!}" id="{{$field}}"  {{$attr or ''}}>{{$title or $field}} </label>
     @endif
     <div class="help-block with-errors"></div>
 </div>

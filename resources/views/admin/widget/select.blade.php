1dcc5d0255242de44f22e7f6bc69043f49661281
Tan Hendra <tanhendra.w@gmail.com>
2017-04-25 23:38:27 +0700

    fix select widget

--- resources/views/admin/widget/select.blade.php
+++ resources/views/admin/widget/select.blade.php
@@ -1,9 +1,12 @@
-
 <div class="form-group">
     <label for="{{$field}}" class="text-capitalize">{{$title or $field}} </label>
     <select name="{{$field}}" id="{{$field}}">
         @foreach($option as $dataOption)
-            <option value="{!! $dataOption['value'] !!}" @if($dataOption['value'] == $data->$field) selected @endif>{!! $dataOption['text'] !!}</option>
+            @if(isset($data))
+                <option value="{!! $dataOption['value'] !!}" @if($dataOption['value'] == $data->$field) selected @endif>{!! $dataOption['text'] !!}</option>
+            @else
+                <option value="{!! $dataOption['value'] !!}">{!! $dataOption['text'] !!}</option>
+            @endif
         @endforeach
     </select>
     <div class="help-block with-errors"></div>
